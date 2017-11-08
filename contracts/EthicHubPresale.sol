a680c616a2b1aa6d18cc534b40c7b0d24dd20816
Ramarti <raul@MacBook-Pro-de-Raul.local>
2017-11-08 13:55:41 +0100

    refactored test for async await and web3 0.2

--- contracts/EthicHubPresale.sol
+++ contracts/EthicHubPresale.sol
@@ -17,9 +17,11 @@ contract EthicHubPresale is CappedCrowdsale, Ownable {
    * @param       _wallet where funds are collected
    */
   function EthicHubPresale(uint256 _startTime, uint256 _endTime, uint256 _goal, uint256 _cap, address _wallet)
+    Ownable()
     CappedCrowdsale(_cap)
     Crowdsale(_startTime, _endTime, 1, _wallet)
   {
+
     //As goal needs to be met for a successful crowdsale
     //the value needs to less or equal than a cap which is limit for accepted funds
     require(_goal <= _cap);
--- test/EthicHubPresale.js
+++ test/EthicHubPresale.js
@@ -5,36 +5,23 @@ var EthicHubPresale = artifacts.require('../contracts/EthicHubPresale.sol');
 
 describe('Initialization', function() {
   contract('EthicHubPresale', function(accounts) {
-    it("should create EthicHubPresale and be the owner", async function() {
-      let instance = await EthicHubPresale.new(
-        Math.floor(Date.now() / 1000),
-        Math.floor(Date.now() / 1000) + 1000,
-        3,
-        4,
-        accounts[1]);
-      console.log(instance);
-      let owner = await instance.owner();
-      console.log(owner);
-      assert.equal(accounts[0], owner);
-    });
-  });
-});
-
-describe('Hard Cap', (accounts) => {
-  //This test is to assure we are inheriting OpenZeppelin CappedCrowdsale
-  contract('EthicHubPresale', function(accounts) {
-    it("should set a hardcap When created", function() {
-      return EthicHubPresale.new(
+    let presale;
+    beforeEach(async function() {
+      presale = await EthicHubPresale.new(
         Math.floor(Date.now() / 1000),
         Math.floor(Date.now() / 1000) + 1000,
         3,
         4,
         accounts[1]
-      ).then( instance => {
-        return instance.cap();
-      }).then( cap => {
-        assert.equal(4, cap);
-      });
+      );
+    });
+    it("should create EthicHubPresale and be the owner", async function() {
+      let owner = await presale.owner();
+      assert.equal(accounts[0], owner);
+    });
+    it("should set a hardcap When created", async function() {
+      let cap = await presale.cap();
+      assert.equal(4, cap);
     });
   });
 });
