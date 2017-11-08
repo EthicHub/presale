ff8684706044c53190ef51dd6424451d388a43ef
Ramarti <raul@MacBook-Pro-de-Raul.local>
2017-11-08 16:12:24 +0100

    added truffle

---
+++ truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 9545,
+      network_id: "*" // Match any network id
+    }
+  },
+  current_network: "development"
+
+};
