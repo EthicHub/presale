105afa96fcd4d772b5f9770c9efcdd4cacde947e
kkx <qijiongjiang1@hotmail.com>
2017-11-15 02:20:48 +0100

    add token vesting contract with its tests

--- truffle.js
+++ truffle.js
@@ -1,8 +1,11 @@
+require('babel-register');
+require('babel-polyfill');
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 9545,
+      port: 8545,
       network_id: "*" // Match any network id
     }
   },
