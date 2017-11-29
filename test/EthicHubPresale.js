34701be04ea280f91656ff343398310ee48611f3
mUniKeS <munikes@members.fsf.org>
2017-11-29 18:18:33 +0100

    Test the EthicHubPresale contract

--- test/EthicHubPresale.js
+++ test/EthicHubPresale.js
@@ -1,27 +1,77 @@
 'use strict';
-//const assertJump = require('./helpers/assertJump');
+import ether from './helpers/ether'
+import {advanceBlock} from './helpers/advanceToBlock'
+import {increaseTimeTo, duration} from './helpers/increaseTime'
+import latestTime from './helpers/latestTime'
+import EVMRevert from './helpers/EVMRevert'
 
-var EthicHubPresale = artifacts.require('../contracts/EthicHubPresale.sol');
+const BigNumber = web3.BigNumber
 
-describe('Initialization', function() {
-  contract('EthicHubPresale', function(accounts) {
-    let presale;
-    beforeEach(async function() {
-      presale = await EthicHubPresale.new(
-        Math.floor(Date.now() / 1000),
-        Math.floor(Date.now() / 1000) + 1000,
-        3,
-        4,
-        accounts[1]
-      );
+const should = require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should()
+
+const FixedPoolWithDiscountsTokenDistributionMock = artifacts.require('./helpers/FixedPoolWithDiscountsTokenDistributionMock');
+const Token = artifacts.require('ERC20')
+
+const SimpleToken = artifacts.require('SimpleToken')
+
+const EthicHubPresale = artifacts.require('EthicHubPresale');
+
+contract('EthicHubPresale', function ([owner, _, investor, wallet]) {
+
+  const RATE = new BigNumber(4000);
+  const cap = ether(3000)
+  const goal = ether(800)
+  const numIntervals = 3;
+  const percentageDiscount = 10;
+
+  before(async function() {
+    //Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+    await advanceBlock();
+  })
+
+
+  beforeEach(async function () {
+    this.startTime = latestTime() + duration.weeks(1);
+    this.endTime = this.startTime + duration.weeks(8);
+
+    const fixedPoolToken = await SimpleToken.new();
+    const totalSupply = await fixedPoolToken.totalSupply();
+    this.tokenDistribution = await FixedPoolWithDiscountsTokenDistributionMock.new(fixedPoolToken.address,RATE);
+
+    this.crowdsale = await EthicHubPresale.new(this.startTime, this.endTime, goal, cap, wallet, this.tokenDistribution.address);
+    await fixedPoolToken.transfer(this.tokenDistribution.address, totalSupply);
+    this.token = Token.at(await this.tokenDistribution.getToken.call());
+
+  })
+
+  describe('Initialization', function() {
+
+    beforeEach(async function () {
+      this.afterEndTime = this.endTime + duration.seconds(1);
+      for (var i = 0; i <= numIntervals; i++) {
+        this.tokenDistribution.addInterval(this.startTime + duration.weeks(2*i+1), (numIntervals-i)*percentageDiscount);
+      }
+    //  await increaseTimeTo(this.startTime)
+    })
+
+    it('should fulfilled initiate with intervals token distribution', async function () {
+      await this.tokenDistribution.initIntervals().should.be.fulfilled;
+    })
+
+    it("should create the owner", async function() {
+      (await this.crowdsale.owner()).should.be.equal(owner);
+    })
+
+    it("should set a cap when created", async function() {
+      (await this.crowdsale.cap()).should.be.bignumber.equal(cap);
     });
-    it("should create EthicHubPresale and be the owner", async function() {
-      let owner = await presale.owner();
-      assert.equal(accounts[0], owner);
-    });
-    it("should set a hardcap When created", async function() {
-      let cap = await presale.cap();
-      assert.equal(4, cap);
+
+    it("should set a goal when created", async function() {
+      (await this.crowdsale.goal()).should.be.bignumber.equal(goal);
     });
   });
+
 });
